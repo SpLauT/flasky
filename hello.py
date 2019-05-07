f881414fc29c18947e704e5be3019dd7ff0c24a8
Max Hansen <splautmail@gmail.com>
2019-05-07 15:43:12 +0200

    This is the dockerization of my flask app, It was pretty awesome, and I learned a lot

--- hello.py
+++ hello.py
@@ -14,7 +14,7 @@ def index():
 @app.route('/user/<string:name>')
 def user(name):
     return render_template('user.html', name=name)
-
+ 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
@@ -22,3 +22,4 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
